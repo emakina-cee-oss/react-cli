f06901403c9981e98742a17f804d9cc240c408b4
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-23 11:07:40 +0200

    align with linting rules

--- generators/app/templates/shared/static/src/components/App/App.js
+++ generators/app/templates/shared/static/src/components/App/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="c-app">
-                <div className="c-app__header">
-                    <img src={logo} className="c-app__logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="c-app__intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+const App = () => {
+    return (
+        <div className="c-app">
+            <div className="c-app__header">
+                <img src={logo} className="c-app__logo" alt="logo" />
+                <h2>Welcome to React</h2>
             </div>
-        );
-    }
-}
+            <p className="c-app__intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    );
+};
 
 export default App;
