0e41dbcb55cd09b56175a7ec52d269b2549b2983
Daniel Goerdes <d.goerdes@emakina.at>
2017-05-23 12:37:52 +0200

    add example service to initial templates

---
+++ generators/app/templates/shared/static/src/shared/services/apiService.js
@@ -0,0 +1,27 @@
+/**
+ * API
+ * This is just an example service to show where services
+ * live in the folder structure.
+ *
+ * Feel free to extend or remove it.
+ */
+class Api {
+
+    get() {
+        return {};
+    }
+
+    put() {
+        return {};
+    }
+
+    post() {
+        return {};
+    }
+
+    delete() {
+        return {};
+    }
+}
+
+export default Api;
