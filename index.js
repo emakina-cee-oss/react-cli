7e22f292c4e90a47ae61e8c9edb9087a00fb6e28
Daniel Goerdes <d.goerdes@emakina.at>
2017-06-30 09:37:05 +0200

    get yeoman generator tasks running from cli

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 #!/usr/bin/env node
+const {join} = require('path');
+const {spawnSync} = require('child_process');
 const program = require('commander');
-const shell = require('shelljs');
 const packageInfo = require('./package.json');
-const childProcess = require('child_process');
-
 
 program.version(packageInfo.version);
-
-program
-    .command('init')
-    .action(() => {
-        shell.exec('npm install -g yo generator-emakinacee-react');
-    });
 
 // NEW COMMAND 'new'
 program
@@ -19,25 +12,17 @@ program
     .option('-s, --spa', 'Create new single page application')
     .option('-y, --yarn', 'Use Yarn')
     .action((name, options) => {
-        const args = [];
-        args.push('emakinacee-react');
-        args.push(name);
+        const args = [
+            'emakinacee-react',
+            name
+        ];
         if (options.spa) args.push('--spa');
         if (options.yarn) args.push('--yarn');
 
-        // console.log(`yo emakinacee-react ${name} ${opts.join(' ')}`);
-        // shell.exec(`C:/Users/daniel.goerdes/AppData/Roaming/npm/node_modules/emakina-react-cli/yo.sh`);
-
-        const child = childProcess.spawn('yo', args, {stdio: 'inherit'});
-        // child.stdout.pipe(process.stdout);
-        // child.stderr.pipe(process.stderr);
-        // process.stdin.pipe(child.stdin);
-        // process.stdin.resume();
-        //
-        // child.on('exit', (code, signal) => {
-        //     process.stdin.pause();
-        //     console.log(arguments);
-        // });
+        spawnSync(join(__dirname, 'node_modules', '.bin', 'yo'), args, {
+            stdio: 'inherit',
+            shell: true
+        });
     });
 
 // GENERATE COMMAND 'g'
@@ -46,15 +31,19 @@ program
     .option('-c, --connect', 'Connect Component to Cerebral')
     .option('--class', 'Generate Component in ES6 Class Syntax')
     .action((type, name, module, options) => {
-        const opts = [];
-        if (options.connect) opts.push('--connect');
-        if (options.class) opts.push('--class');
+        const args = [
+            'emakinacee-react:gen',
+            type,
+            name
+        ];
+        if (module) args.push(module);
+        if (options.connect) args.push('--connect');
+        if (options.class) args.push('--class');
 
-        let commandString = `yo emakinacee-react:gen ${type} ${name}`;
-        if (module) commandString += ` ${module}`;
-
-        // console.log(`${commandString} ${opts.join(' ')}`);
-        shell.exec(`${commandString} ${opts.join(' ')}`);
+        spawnSync(join(__dirname, 'node_modules', '.bin', 'yo'), args, {
+            stdio: 'inherit',
+            shell: true
+        });
     });
 
 program.parse(process.argv);
