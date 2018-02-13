d7468234e0ecffd4757d7f51101966223995814b
Daniel Gördes <me@dgoerdes.de>
2018-02-13 10:40:08 +0100

    [generator-emakinacee-react][#17] add example for classnames and usage of shared style modules

--- packages/generator-emakinacee-react/generators/app/templates/static/src/containers/Home/Home.js
+++ packages/generator-emakinacee-react/generators/app/templates/static/src/containers/Home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import classNames from 'classnames';
 import { connect } from '@cerebral/react';
 import { state } from 'cerebral/tags';
 import Button from '../../components/Button/Button';
+import flexStyles from '../../styles/modules/flex.module.scss';
+import containerStyles from '../../styles/modules/container.module.scss';
 import styles from './Home.module.scss';
 
 const Home = ({ title }) => {
@@ -11,7 +14,21 @@ const Home = ({ title }) => {
             <p className={styles.intro}>
                 To get started, edit <code>src/App.js</code> and save to reload.
             </p>
-            <Button label="Foo" />
+
+            <div
+                className={classNames(
+                    containerStyles.container,
+                    flexStyles.flex,
+                    flexStyles.flexJustifyBetween
+                )}
+            >
+                <div>
+                    <Button label="Foo" /> is so ...
+                </div>
+                <div>
+                    ... damn flexible!
+                </div>
+            </div>
         </div>
     );
 };
