61133703695a556f4efdb4bfc234ff42082d2bf0
Flo Bacher <f.bacher@emakina.at>
2018-07-06 19:20:45 +0200

    [generator-emakinacee-react] usage of babelrc breaks the build somehow

--- packages/generator-emakinacee-react/generators/app/templates/static-ts/config-overrides.js
+++ packages/generator-emakinacee-react/generators/app/templates/static-ts/config-overrides.js
@@ -11,17 +11,10 @@ const {
 } = require('react-app-rewire-typescript-babel-preset');
 
 const {
-    getBabelLoader,
     getLoader,
     loaderNameMatches,
 } = require('react-app-rewired');
 
-
-function rewireBabelLoaderToUseBabelRC(config, env) {
-    const babelLoader = getBabelLoader(config.module.rules);
-    babelLoader.options.babelrc = true;
-    return config;
-}
 const rewireBabelLoaderForDependencies = require('./config/rewireBabelLoaderForDependencies');
 const rewireESLint = require('./config/rewireESLint');
 
@@ -29,7 +22,6 @@ module.exports = {
     webpack: function (config, env) {
         config = rewireBabelLoaderForDependencies(config, env);
         config = rewireTypescript(config, env);
-        config = rewireBabelLoaderToUseBabelRC(config, env);
         config = rewireESLint(config);
         return config;
     },
