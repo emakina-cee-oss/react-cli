414f9850ae5a86c661d3be74058bb629636bf0a1
Flo Bacher <f.bacher@emakina.at>
2018-07-09 17:03:31 +0200

    [generator-emakinacee-react] this will not report errors via eslint anymore

--- packages/generator-emakinacee-react/generators/app/templates/static-ts/src/components/Button/Button.tsx
+++ packages/generator-emakinacee-react/generators/app/templates/static-ts/src/components/Button/Button.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-// eslint-disable-next-line
 type ButtonProps = { label: string };
 
 const Button: React.StatelessComponent<ButtonProps> = ({ label }: ButtonProps) => {
